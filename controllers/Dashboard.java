2278e80425e823ffb6bf943262c3024d9e8b1185
Hager Albadri <69991638+hbadri02@users.noreply.github.com>
2021-01-16 15:13:40 -0500

    Listing the top 20 songs
    Dashboard list works now

--- controllers/Dashboard.java
+++ controllers/Dashboard.java
@@ -7,39 +7,58 @@ package controllers;
 
 
 import bluey.FXMLDocumentController;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import models.Genius;
+import models.Shazam;
 import models.User;
 
 /**
  *
  * @author HBadr
  */
-public class Dashboard {
+public class Dashboard implements Initializable{
     @FXML private  TextField favArtist;
     @FXML private Button choose;
     @FXML private WebView favArtistWebView; 
-    public String artistChoice;
+    @FXML private ListView listy;
+    public String nameWithoutSpaces;
     public static FXMLDocumentController tas;
     //private User USER = FXMLDocumentController.getUser();
     String unamee = bluey.FXMLDocumentController.uname;
+    private LinkedList<String> top10SongsList = models.Shazam.top10SongsList;
     
      @FXML
     private void handleChooseButton(ActionEvent event){
-        this.artistChoice = this.favArtist.getText();
-        String nameWithoutSpaces = artistChoice.replaceAll(" ", "%20");
+        String artistChoice = this.favArtist.getText();
+         this.nameWithoutSpaces = artistChoice.replaceAll(" ", "%20");
         Genius one = new Genius(nameWithoutSpaces);
         WebEngine web = favArtistWebView.getEngine();
         String image = one.artistPicture();
         web.load(image);
         User.chooseFavoriteArtist(unamee, artistChoice);
-        
-        
+        populateSongsList();   
+    }
+    public void populateSongsList() {
+        User tempUser = new User();
+        String pathFromUser = tempUser.getFavoriteArtist();
+        String nameWithoutSpacesForList = pathFromUser.replaceAll(" ", "%20");
+        Shazam two = new Shazam ();
+        two.populatetop10SongsList(listy, nameWithoutSpacesForList);
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        populateSongsList();
     }
 
 }
--- models/Shazam.java
+++ models/Shazam.java
@@ -6,25 +6,37 @@
 package models;
 
 import java.util.LinkedList;
+import javafx.scene.control.ListView;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 
 /**
  *
  * @author HBadr
  */
 public class Shazam {
+
     private final String key = "ee0b3a7e09mshb39bb62b99ec98ep1662d5jsn43647b0343b6";
     private final String host = "shazam.p.rapidapi.com";
     private final String idURL = "https://shazam.p.rapidapi.com/search?term=";
     private final String topSongsURL = "https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?id=";
-    public final LinkedList<String> top10SongsList = new LinkedList();
+    public final static LinkedList<String> top10SongsList = new LinkedList();
     private JSONArray artists;
-    private final JSONObject track;
+    private JSONObject track;
     private int idNumber;
-    
-    public Shazam(String name){
+    private ListView list;
+    private JSONArray array;
+
+    public Shazam() {
+
+    }
+
+    /**
+     *
+     * @param listy
+     * @param name
+     */
+    private void Shazam(String name) {
         String url = idURL + name + "&locale=en-US&offset=0&limit=5";
         ConnectionToRapidAPI api = new ConnectionToRapidAPI(url, key, host);
         JSONObject job = api.getJsonObject();
@@ -32,25 +44,39 @@ public class Shazam {
         JSONArray array = tracks.getJSONArray("hits");
         JSONObject temp = array.getJSONObject(0);
         this.track = temp.getJSONObject("track");
-        top10Songs(getIDNumberFromName());   
+        top10Songs(getIDNumberFromName());
     }
-    
-    private int getIDNumberFromName(){
-      this.artists = this.track.getJSONArray("artists");
+
+    private int getIDNumberFromName() {
+        this.artists = this.track.getJSONArray("artists");
         JSONObject id = artists.getJSONObject(0);
         this.idNumber = id.getInt("id");
         return idNumber;
     }
-    
-    private void top10Songs(int id){
+
+    public void top10Songs(int id) {
+        id = this.idNumber;
         String url = topSongsURL + id + "&locale=en-US";
         ConnectionToRapidAPI api = new ConnectionToRapidAPI(url, key, host);
         JSONObject job = api.getJsonObject();
-        JSONArray array = job.getJSONArray("tracks");
-        for (int count = 0; count < 9; count++){
-        JSONObject temp = array.getJSONObject(count);
-        String teme = temp.getString("title");
-        top10SongsList.add(teme);          
+        this.array = job.getJSONArray("tracks");
+
     }
+
+    public void populatetop10SongsList(ListView listy, String name) {
+        Shazam(name);
+        this.list = listy;
+        top10SongsList.clear();
+        listy.getItems().removeAll();
+        listy.getItems().clear();
+        for (int count = 0; count < 20; count++) {
+            JSONObject temp = array.getJSONObject(count);
+            String teme = temp.getString("title");
+            top10SongsList.add(teme);
+        }
+        for (int i = 0; i < this.top10SongsList.size(); i++) {
+            listy.getItems().add(this.top10SongsList.get(i));
+        }
+
     }
 }
